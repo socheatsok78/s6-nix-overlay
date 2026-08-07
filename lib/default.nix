24ceec2904209ec98cc49111a474bd1423aa91d5
Socheat <github@socheat.dev>
2026-08-07 19:09:45 +0700

    renamed: lib.s6-overlay-tools to lib.dockerTools

--- lib/default.nix
+++ lib/default.nix
@@ -9,7 +9,7 @@ with pkgs.lib;
   #
   # hexint = x: hexvals.${toLower x};
 
-  s6-overlay-tools = import ./s6-overlay-tools {
+  dockerTools = import ./dockerTools {
     inherit
       pkgs
       s6-overlay
