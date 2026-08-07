824708d1bea74a57fca1460c45445d27da04c770
Socheat <github@socheat.dev>
2026-08-07 16:02:40 +0700

    refactor packages and add s6-overlay-tools

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
   outputs =
     { self, nixpkgs }:
     let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
@@ -20,11 +25,8 @@
         }
       );
 
-      packages = forAllSystems (
-        system:
-        import ./default.nix {
-          pkgs = import nixpkgs { inherit system; };
-        }
+      packages = forAllSupportedSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
       );
 
       # nix fmt (experimental)
--- lib/default.nix
+++ lib/default.nix
@@ -5,4 +5,6 @@ with pkgs.lib;
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+
+  s6-overlay-tools = import ./s6-overlay-tools.nix { inherit pkgs; };
 }
