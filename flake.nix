21bd03f9293ec6e311a313d7d1b64fb7215138be
Socheat <github@socheat.dev>
2026-08-11 16:45:33 +0700

    update supported systems

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
     let
       supportedSystems = [
         "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+        "i686-linux"
+        "powerpc64le-linux"
+        "riscv64-linux"
         "x86_64-linux"
       ];
       forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
